a99f8fc6e6f141df1b4c7384d412430d4ad3d1e5
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-07-05 14:58:27 +0200

    efl_ui_range_display: add a spec testsuite for range_display
    this also fixes the widgets that did not follow this specification.

--- src/tests/elementary/efl_ui_test_spin.c
+++ src/tests/elementary/efl_ui_test_spin.c
@@ -63,17 +63,35 @@ EFL_END_TEST
 
 EFL_START_TEST (spin_wheel_test)
 {
+   Eina_Bool changed = EINA_FALSE, min_reached = EINA_FALSE, max_reached = EINA_FALSE;
+
    efl_ui_range_limits_set(spin, -100.0, 100.0);
    efl_ui_range_value_set(spin, 0.0);
    efl_ui_range_step_set(spin, 10.0);
+   efl_event_callback_add(spin, EFL_UI_SPIN_EVENT_CHANGED, _set_flag, &changed);
+   efl_event_callback_add(spin, EFL_UI_SPIN_EVENT_MIN_REACHED, _set_flag, &min_reached);
+   efl_event_callback_add(spin, EFL_UI_SPIN_EVENT_MAX_REACHED, _set_flag, &max_reached);
+
    efl_gfx_entity_size_set(win, EINA_SIZE2D(60, 60));
    get_me_to_those_events(spin);
    evas_event_feed_mouse_move(evas_object_evas_get(spin), 30, 30, 1234, NULL);
-   ck_assert(efl_ui_range_value_get(spin) == 0.0);
    evas_event_feed_mouse_wheel(evas_object_evas_get(spin), -1, -1, 12345, NULL);
    ck_assert(efl_ui_range_value_get(spin) == 10.0);
+   ck_assert_int_eq(changed, EINA_TRUE);
+   ck_assert_int_eq(min_reached, EINA_FALSE);
+   ck_assert_int_eq(max_reached, EINA_FALSE);
+   changed = EINA_FALSE;
+   min_reached = EINA_FALSE;
+   max_reached = EINA_FALSE;
+
    evas_event_feed_mouse_wheel(evas_object_evas_get(spin), -1, 1, 12345, NULL);
    ck_assert(efl_ui_range_value_get(spin) == 0.0);
+   ck_assert_int_eq(changed, EINA_TRUE);
+   ck_assert_int_eq(min_reached, EINA_FALSE);
+   ck_assert_int_eq(max_reached, EINA_FALSE);
+   changed = EINA_FALSE;
+   min_reached = EINA_FALSE;
+   max_reached = EINA_FALSE;
 }
 EFL_END_TEST
 
